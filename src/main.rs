92660ec588b47c1699a96ef76ff4f2f85494b62c
Antoine Latrille <antoine.latrille@gmail.com>
2020-04-19 22:45:49 +0200

    Create GameState struct to hold the game state and implement the iteration function

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,64 @@ use clap::{Arg, App, value_t_or_exit};
 use std::io::{BufWriter, Write};
 use std::io;
 
-fn new_state(x: usize, y: usize) -> Vec<Vec<bool>> {
-    let mut xs = Vec::<Vec<bool>>::with_capacity(x);
-    // let mut is_even = true;
-    for i in 0..x {
-        let v = i == 2;
-        xs.push((0..y).map(|_| v).collect());
-        // is_even = !is_even;
-    }
-    return xs;
+const EMPTY_STR: &[u8] = " ".as_bytes();
+
+const CAR_RET: &[u8] = "\n".as_bytes();
+const X: &[u8] = "x".as_bytes();
+const DASH: &[u8] = "-".as_bytes();
+
+struct GameState {
+    grid: Vec<Vec<bool>>,
+    x: usize,
+    y: usize,
 }
 
-const EMPTY_STR : &[u8] = " ".as_bytes();
-const CAR_RET : &[u8] = "\n".as_bytes();
-const X : &[u8] = "x".as_bytes();
-const DASH : &[u8] = "-".as_bytes();
+impl GameState {
+    fn new(x: usize, y: usize) -> GameState {
+        let mut grid = Vec::<Vec<bool>>::with_capacity(x);
+        for i in 0..x {
+            let v = i == 2;
+            grid.push((0..y).map(|_| v).collect());
+        };
+        return GameState {
+            grid,
+            x,
+            y,
+        };
+    }
 
-fn render(state: &Vec<Vec<bool>>, max_x: usize, max_y: usize, writer: &mut dyn Write) {
-    (0..max_y).for_each(|y| {
-        (0..max_x).for_each(|x| {
-            writer.write(if state[x][y] { X } else { DASH });
-            if x < (max_x - 1) {
-                writer.write(EMPTY_STR);
-            }
-        });
-        writer.write(CAR_RET);
-    });
+    fn next(self: GameState) -> GameState {
+        let new_grid: Vec<Vec<bool>> = (0..self.x).map(|x| {
+            (0..self.y).map(|y| {
+                let mut neighbors: u8 = 0;
+                // add x - 1 neighbors (left neighbors)
+                if x > 0 {
+                    neighbors += if y > 0 && self.grid[x - 1][y - 1] { 1 } else { 0 };
+                    neighbors += if self.grid[x - 1][y] { 1 } else { 0 };
+                    neighbors += if y < (self.y - 1) && self.grid[x - 1][y + 1] { 1 } else { 0 };
+                }
+
+                // add x neighbors (top and bottom)
+                neighbors += if y > 0 && self.grid[x][y - 1] { 1 } else { 0 };
+                neighbors += if y < (self.y - 1) && self.grid[x][y + 1] { 1 } else { 0 };
+
+                // add x + 1 neighbors (right neighbors)
+                if x < self.x - 1 {
+                    neighbors += if y > 0 && self.grid[x + 1][y - 1] { 1 } else { 0 };
+                    neighbors += if self.grid[x + 1][y] { 1 } else { 0 };
+                    neighbors += if y < (self.y - 1) && self.grid[x + 1][y + 1] { 1 } else { 0 };
+                }
+
+                return (self.grid[x][y] && (neighbors == 2 || neighbors == 3)) || (!self.grid[x][y] && neighbors == 3);
+            }).collect()
+        }).collect();
+
+        return GameState {
+            grid: new_grid,
+            x: self.x,
+            y: self.y,
+        };
+    }
 }
 
 fn main() {
@@ -38,7 +70,7 @@ fn main() {
         .arg(Arg::with_name("x")
             .help("horizontal length of the board")
             .required(true)
-            .default_value("30")
+            .default_value("20")
             .index(1))
         .arg(Arg::with_name("y")
             .help("vertical length of the board")
@@ -48,10 +80,24 @@ fn main() {
     let x = value_t_or_exit!(matches, "x", usize);
     let y = value_t_or_exit!(matches, "y", usize);
 
-    let state = new_state(x, y);
-
-    println!("Render of a base state for x={0}, y={1}: ", x, y);
+    let state = GameState::new(x, y);
     let mut stdout = BufWriter::new(io::stdout());
 
-    render(&state, x, y, &mut stdout);
+    println!("Render of a base state for x={0}, y={1}: ", x, y);
+    render(&state, &mut stdout);
+    println!("\nNext iteration:");
+    render(&(state.next()), &mut stdout);
+}
+
+
+fn render(state: &GameState, writer: &mut dyn Write) {
+    (0..state.y).for_each(|y| {
+        (0..state.x).for_each(|x| {
+            writer.write(if state.grid[x][y] { X } else { DASH }).unwrap();
+            if x < (state.x - 1) {
+                writer.write(EMPTY_STR).unwrap();
+            }
+        });
+        writer.write(CAR_RET).unwrap();
+    });
 }
