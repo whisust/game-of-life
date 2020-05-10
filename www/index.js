36a68085f9d75dd78926e8b72d5678f811ccd765
Antoine Latrille <antoine.latrille@gmail.com>
2020-05-10 21:28:37 +0200

    Add www directory and first implementation of the front

---
+++ www/index.js
@@ -0,0 +1,43 @@
+import {State} from "game-of-life";
+
+const game = State.new(10, 5);
+const pre = document.getElementById('game-of-life-canvas');
+
+let fpsInterval, startTime, now, then, elapsed;
+
+// initialize the timer variables and start the animation
+
+function startAnimating(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+}
+
+function animate() {
+
+    // request another frame
+
+    requestAnimationFrame(animate);
+
+    // calc elapsed time since last loop
+
+    now = Date.now();
+    elapsed = now - then;
+
+    // if enough time has elapsed, draw the next frame
+
+    if (elapsed > fpsInterval) {
+
+        // Get ready for next frame by setting then=now, but also adjust for your
+        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+        then = now - (elapsed % fpsInterval);
+
+        // Put your drawing code here
+        pre.textContent = game.render();
+        game.next();
+    }
+}
+
+
+startAnimating(5);
