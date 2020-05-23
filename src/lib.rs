3fdb7b99e05682023a47aa6776b3d92f88c45ac5
Antoine Latrille <antoine.latrille@gmail.com>
2020-05-23 15:20:29 +0200

    Add Patterns + a way to add patterns in a game

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,39 @@ pub struct State {
     pub generation: u32,
 }
 
+trait PatternIndexes {
+    fn cells(pattern: Pattern, x: usize, y: usize) -> Vec<(usize, usize, bool)>;
+}
+
+#[wasm_bindgen]
+pub enum Pattern {
+    Glider,
+    Pulsar,
+}
+
+impl PatternIndexes for Pattern {
+    fn cells(pattern: Pattern, x: usize, y: usize) -> Vec<(usize, usize, bool)> {
+        match pattern {
+            Pattern::Glider => [
+                (x - 1, y - 1, false), (x, y - 1, true), (x + 1, y - 1, false),
+                (x - 1, y, false), (x, y, false), (x + 1, y, true),
+                (x - 1, y + 1, true), (x, y + 1, true), (x + 1, y + 1, true),
+            ].to_vec(),
+            _ => Vec::<(usize, usize, bool)>::new()
+        }
+    }
+}
+
+// impl PatternIndexes for Pattern::Pulsar {
+//     fn cells(x: usize, y: usize) -> *[(usize, usize, bool)] {
+//         return *[
+//             (x - 1, y + 1, false), (x, y + 1, true), (x + 1, y + 1, true),
+//             (x - 1, y, false), (x, y, false), (x + 1, y, true),
+//             (x - 1, y - 1, true), (x, y - 1, true), (x + 1, y - 1, true),
+//         ];
+//     }
+// }
+
 
 #[wasm_bindgen]
 impl State {
@@ -47,7 +80,7 @@ impl State {
     pub fn new(width: usize, height: usize) -> State {
         log!("Generating new state with dimensions {} x {}", width, height);
         utils::set_panic_hook(); // we have to plug this in a common code path
-        let mut grid = FixedBitSet::with_capacity(width * height);
+        let grid = FixedBitSet::with_capacity(width * height);
         return State {
             grid,
             width,
@@ -129,6 +162,10 @@ impl State {
         let idx = self.get_index(row, column);
         self.grid.toggle(idx);
     }
+
+    pub fn add_pattern(&mut self, row: usize, column: usize, pattern: Pattern) {
+        self.set_pattern(Pattern::cells(pattern, row, column))
+    }
 }
 
 impl State {
@@ -136,10 +173,21 @@ impl State {
         &self.grid.as_slice()
     }
 
-    pub fn set_cells(&mut self, cells: &[(usize, usize)]) {
+    pub fn set_cells(&mut self, cells: &[(usize, usize)], enabled: bool) {
         for (row, col) in cells.iter().cloned() {
-            let idx = self.get_index(row, col);
-            self.grid.set(idx, false);
+            if row <= self.width && col <= self.height {
+                let idx = self.get_index(row, col);
+                self.grid.set(idx, enabled);
+            }
+        }
+    }
+
+    pub fn set_pattern(&mut self, cells: Vec<(usize, usize, bool)>) {
+        for (row, col, enabled) in cells.iter().cloned() {
+            if row <= self.width && col <= self.height {
+                let idx = self.get_index(row, col);
+                self.grid.set(idx, enabled);
+            }
         }
     }
 }
--- tests/web.rs
+++ tests/web.rs
@@ -15,14 +15,14 @@ wasm_bindgen_test_configure!(run_in_browser);
 #[cfg(test)]
 pub fn input_spaceship() -> State {
     let mut state = State::new(6, 6);
-    state.set_cells(&[(1, 2), (2, 3), (3, 1), (3, 2), (3, 3)]);
+    state.set_cells(&[(1, 2), (2, 3), (3, 1), (3, 2), (3, 3)], true);
     state
 }
 
 #[cfg(test)]
 pub fn expected_spaceship() -> State {
     let mut state = State::new(6, 6);
-    state.set_cells(&[(2, 1), (2, 3), (3, 2), (3, 3), (4, 2)]);
+    state.set_cells(&[(2, 1), (2, 3), (3, 2), (3, 3), (4, 2)], true);
     state
 }
 
